8cb6a5990200452103528c88209e2a5dd9e96d18
Mike Mundy <mundy.michael@mayo.edu>
2016-11-11 13:04:31 -0600

    Finish implementation of compare_models() function, general cleanup

--- cobra/oven/mmundy42/analysis.py
+++ cobra/oven/mmundy42/analysis.py
@@ -1,7 +1,7 @@
 from six import iteritems, print_
 import pandas as pd
 from tabulate import tabulate
-
+from cobra.core import DictList
 
 """
 Notes
@@ -18,15 +18,15 @@ def format_long_string(string, max_length):
 
     Parameters
     ----------
-        string : str
-            String to format
-        max_length : int
-            Maximum length of returned string
+    string : str
+        String to format
+    max_length : int
+        Maximum length of returned string
 
     Returns
     -------
-        str
-            Formated string
+    str
+        Formated string
     """
 
     if len(string) > max_length:
@@ -40,18 +40,18 @@ def metabolites_consumed(model, threshold=1E-8, floatfmt='.3f'):
 
     Parameters
     ----------
-        model
-            Model to analyze (must be optimized)
-        threshold : float)
-            Tolerance for determining if a flux is zero
-        floatfmt : str
-            Format for floats when printed by tabulate
+    model : cobra.Model
+        Model to analyze (must be optimized)
+    threshold : float, optional
+        Tolerance for determining if a flux is zero
+    floatfmt : str, optional
+        Format for floats when printed by tabulate
     """
 
     # Build a dictionary of metabolite consumption and production from the boundary reactions.
-    boundary_reactions = model.reactions.query(lambda x: x, 'boundary')
+    reactions = model.reactions.query(lambda x: x, 'boundary')
     metabolite_fluxes = {}
-    for rxn in boundary_reactions:
+    for rxn in reactions:
         for met, stoich in iteritems(rxn.metabolites):
             metabolite_fluxes[met] = {
                 'id': format_long_string(met.id, 15),
@@ -72,6 +72,7 @@ def metabolites_consumed(model, threshold=1E-8, floatfmt='.3f'):
     consumed = consumed.sort_values(by=['flux', 'id'], ascending=[False, True])
 
     # Print a nice table.
+    print_('{0} metabolites consumed by {1}\n'.format(len(consumed.index), model.id))
     print_(tabulate(consumed.loc[:, ['id', 'flux', 'name']].values,
                     floatfmt=floatfmt, tablefmt='simple', headers=['ID', 'FLUX', 'NAME']))
 
@@ -83,19 +84,19 @@ def metabolites_produced(model, threshold=1E-8, floatfmt='.3f'):
 
     Parameters
     ----------
-        model
-            Model to analyze (must be optimized)
-        threshold : float, optional
-            Tolerance for determining if a flux is zero
-        floatfmt : str, optional
-            Format for floats when printed by tabulate
+    model : cobra.Model
+        Model to analyze (must be optimized)
+    threshold : float, optional
+        Tolerance for determining if a flux is zero
+    floatfmt : str, optional
+        Format for floats when printed by tabulate
 
     """
 
     # Build a dictionary of metabolite consumption and production from the boundary reactions.
-    boundary_reactions = model.reactions.query(lambda x: x, 'boundary')
+    reactions = model.reactions.query(lambda x: x, 'boundary')
     metabolite_fluxes = {}
-    for rxn in boundary_reactions:
+    for rxn in reactions:
         for met, stoich in iteritems(rxn.metabolites):
             metabolite_fluxes[met] = {
                 'id': format_long_string(met.id, 15),
@@ -123,138 +124,135 @@ def metabolites_produced(model, threshold=1E-8, floatfmt='.3f'):
     return
 
 
-def exchange_reactions(model):
-    """ Print the exchange reactions in a model with id, name, and definition.
+def boundary_reactions(model):
+    """ Print the boundary reactions in a model with id, name, and definition.
 
     Parameters
     ----------
-        model
-            Model to analyze
+    model : cobra.Model
+        Model to analyze
     """
 
-    boundary_reactions = model.reactions.query(lambda x: x, 'boundary')
-    output = [[rxn.id, rxn.name, rxn.reaction] for rxn in boundary_reactions]
+    reactions = model.reactions.query(lambda x: x, 'boundary')
+    reactions.sort(key=lambda x: x.id)
+    print_('{0} boundary reactions in {1}\n'.format(len(reactions), model.id))
+    output = [[rxn.id, rxn.name, rxn.reaction] for rxn in reactions]
     print_(tabulate(output, tablefmt='simple', headers=['ID', 'NAME', 'DEFINITION']))
 
     return
 
-def compare_models():
-    parser = argparse.ArgumentParser(prog='compare-models')
-    parser.add_argument('model1', help='path to sbml file of first model')
-    parser.add_argument('model2', help='path to sbml file of second model')
-    parser.add_argument('--source1', help='source system that generated first model', default='modelbuilder')
-    parser.add_argument('--source2', help='source system that generated second model', default='modelseed')
-    parser.add_argument('--detail', help='show details on reaction and metabolite differences', action='store_true', default=False)
-    parser.add_argument('--boundary', help='compare boundary reactions', action='store_true', default=False)
-    args = parser.parse_args()
 
-    # Read the SBML files for the two models to compare.
-    print 'Reading first model from {0}'.format(args.model1)
-    model1 = cobra.io.read_sbml_model(args.model1)
-    print 'Reading second model from {0}'.format(args.model2)
-    model2 = cobra.io.read_sbml_model(args.model2)
+def exchange_reactions(model):
+    """ Print the exchange reactions in a model with id, name, and definition.
 
-    # A model created by the ModelSEED service includes a compartment index number at the end of
-    # every reaction ID and metabolite ID. Remove the compartment index number so comparisons by
-    # ID do not adjustment later.
-    if args.source1 == 'modelseed':
-        trim_index(model1)
+        An exchange reaction is defined as a reaction with an ID that starts with 'EX_'.
 
-    if args.source2 == 'modelseed':
-        trim_index(model2)
-        # trim_index = re.compile(r'[\d]+$')
-        # for r2 in model2.reactions:
-        #     r2.id = re.sub(trim_index, '', r2.id)
-        # model2.repair()
+    Parameters
+    ----------
+    model : cobra.Model
+        Model to analyze
+    """
+
+    reactions = model.reactions.query(lambda x: x.startswith('EX_'), 'id')
+    reactions.sort(key=lambda x: x.id)
+    print_('{0} exchange reactions in {1}\n'.format(len(reactions), model.id))
+    output = [[rxn.id, rxn.name, rxn.reaction] for rxn in reactions]
+    print_(tabulate(output, tablefmt='simple', headers=['ID', 'NAME', 'DEFINITION']))
+
+    return
+
+
+def compare_models(model1, model2, detail=False, boundary=False):
+    """ Compare two models and report differences.
+
+    Parameters
+    ----------
+    model1 : cobra.Model
+        First model to analyze
+    model2 : cobra.Model
+        Second model to analyze
+    detail : bool, optional
+        When true, print details on differences
+    boundary : bool, optional
+        When true, print info about boundary reactions
+    """
+
+    header = ['ID', 'NAME']
 
     # Compare reactions.
-    print '{0} reactions in first model'.format(len(model1.reactions))
-    print '{0} reactions in second model'.format(len(model2.reactions))
+    print_('REACTIONS\n' + '---------')
+    print_('{0} reactions in {1}'.format(len(model1.reactions), model1.id))
+    print_('{0} reactions in {1}\n'.format(len(model2.reactions), model2.id))
 
     # See if reactions from first model are in the second model.
     num_matched = 0
-    num_only_in_one = 0
+    reaction_only_in_one = DictList()
     for r1 in model1.reactions:
-        try:
-            lookup = r1.id #+'0' # Need to adjust based on source options
-            r2 = model2.reactions.get_by_id(lookup)
+        if model2.reactions.has_id(r1.id):
             num_matched += 1
-        except KeyError:
-            num_only_in_one += 1
-            if args.detail:
-                print 'Reaction {0} is not in second model'.format(lookup)
-    print '{0} reactions in both models'.format(num_matched)
-    print '{0} reactions only in first'.format(num_only_in_one)
+        else:
+            reaction_only_in_one.append(r1)
+    print_('{0} reactions in {1} and {2}'.format(num_matched, model1.id, model2.id))
+    print_('{0} reactions only in {1}\n'.format(len(reaction_only_in_one), model1.id))
+    if detail:
+        output = [[rxn.id, rxn.name] for rxn in reaction_only_in_one]
+        print_(tabulate(output, tablefmt='simple', headers=header) + '\n')
 
     # See if reactions from second model are in the first model.
     num_matched = 0
-    num_only_in_two = 0
+    reaction_only_in_two = DictList()
     for r2 in model2.reactions:
-        try:
-            lookup = r2.id[:-1] # Need to adjust based on source options
-            r1 = model1.reactions.get_by_id(lookup)
+        if model1.reactions.has_id(r2.id):
             num_matched += 1
-        except KeyError:
-            num_only_in_two += 1
-            if args.detail:
-                print 'Reaction {0} is not in first model'.format(lookup)
-    print '{0} reactions in both models'.format(num_matched)
-    print '{0} reactions only in second'.format(num_only_in_two)
+        else:
+            reaction_only_in_two.append(r2)
+    print_('{0} reactions in both {1} and {2}'.format(num_matched, model2.id, model1.id))
+    print_('{0} reactions only in {1}\n'.format(len(reaction_only_in_two), model2.id))
+    if detail:
+        output = [[rxn.id, rxn.name] for rxn in reaction_only_in_two]
+        print_(tabulate(output, tablefmt='simple', headers=header) + '\n')
 
     # Compare metabolites.
-    print '{0} metabolites in first model'.format(len(model1.metabolites))
-    print '{0} metabolites in second model'.format(len(model2.metabolites))
+    print_('METABOLITES\n' + '-----------')
+    print_('{0} metabolites in {1}'.format(len(model1.metabolites), model1.id))
+    print_('{0} metabolites in {1}\n'.format(len(model2.metabolites), model2.id))
 
     # See if metabolites from first model are in the second model.
     num_matched = 0
-    num_only_in_one = 0
+    metabolite_only_in_one = DictList()
     for m1 in model1.metabolites:
-        try:
-            if m1.id.endswith('_b'): # Need to adjust based on source options
-                lookup = m1.id
-            else:
-                lookup = m1.id+'0'
-            m2 = model2.metabolites.get_by_id(lookup)
+        if model2.metabolites.has_id(m1.id):
             num_matched += 1
-        except KeyError:
-            num_only_in_one += 1
-            if args.detail:
-                print 'Metabolite {0} is not in second model'.format(lookup)
-    print '{0} metabolites in both models'.format(num_matched)
-    print '{0} metabolites only in first'.format(num_only_in_one)
+        else:
+            metabolite_only_in_one.append(m1)
+    print_('{0} metabolites in both {1} and {2}'.format(num_matched, model1.id, model2.id))
+    print_('{0} metabolites only in {1}\n'.format(len(metabolite_only_in_one), model1.id))
+    if detail:
+        output = [[met.id, met.name] for met in metabolite_only_in_one]
+        print_(tabulate(output, tablefmt='simple', headers=header) + '\n')
 
     # See if metabolites from second model are in the first model.
     num_matched = 0
-    num_only_in_two = 0
+    metabolite_only_in_two = DictList()
     for m2 in model2.metabolites:
-        try:
-            if m2.id.endswith('_b'): # Need to adjust based on source options
-                lookup = m2.id
-            else:
-                lookup = m2.id[:-1]
-            m1 = model1.metabolites.get_by_id(lookup)
+        if model1.metabolites.has_id(m2.id):
             num_matched += 1
-        except KeyError:
-            num_only_in_two += 1
-            if args.detail:
-                print 'Metabolite {0} is not in first model'.format(lookup)
-    print '{0} metabolites in both models'.format(num_matched)
-    print '{0} metabolites only in second'.format(num_only_in_two)
+        else:
+            metabolite_only_in_two.append(m2)
+    print_('{0} metabolites in both {1} and {2}'.format(num_matched, model2.id, model1.id))
+    print_('{0} metabolites only in {1}\n'.format(len(metabolite_only_in_two), model2.id))
+    if detail:
+        output = [[met.id, met.name] for met in metabolite_only_in_two]
+        print_(tabulate(output, tablefmt='simple', headers=header) + '\n')
 
     # See about system boundary reactions.
-    if args.boundary:
+    if boundary:
         # Get the list of system boundary reactions from first model.
-        model1_boundary = list()
-        for r1 in model1.reactions:
-            if r1.boundary == 'system_boundary':
-                model1_boundary.append(r1)
-        print '{0} reactions are system boundary reactions in first model'.format(len(model1_boundary))
+        model1_boundary = model1.reactions.query(lambda x: x, 'boundary')
+        print_('{0} reactions are system boundary reactions in {1}'.format(len(model1_boundary), model1.id))
 
         # Get the list of system boundary reactions from second model.
-        model2_boundary = list()
-        for r2 in model2.reactions:
-            if r2.boundary == 'system_boundary':
-                model2_boundary.append(r1)
-        print '{0} reactions are system boundary reactions in second model'.format(len(model2_boundary))
+        model2_boundary = model2.reactions.query(lambda x: x, 'boundary')
+        print_('{0} reactions are system boundary reactions in {1}'.format(len(model2_boundary), model2.id))
 
     return
